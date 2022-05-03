df0698f8c84fbe079bc1437c5ef96f2a8d3377ec
Osvaldo <osvruizgar@gmail.com>
2022-05-03 13:48:12 -0400

    finished on Medium Challenge OR

--- main.js
+++ main.js
@@ -23,3 +23,32 @@ function oddOrEven (numbers) {
   console.log(oddOrEven([12, 23, 49, 32, 27]));
   console.log(oddOrEven([11,4,2,12,15]));
   
+  // Medium
+  const car = {
+    make: "toyota",
+    model: "GR86",
+    year: 2020,
+    mileage: 3000,
+    color: "Neptune Blue",
+    driveToWork: function() {
+        console.log("Old Mileage: " + this.mileage);
+
+        this.mileage = this.mileage + 33;
+
+        console.log("New Mileage: " + this.mileage);
+      },
+
+      driveAroundWorld: function() {
+        console.log("Old Mileage: " + this.mileage);
+
+        this.mileage = this.mileage + 24000;
+
+        console.log("New Mileage: " + this.mileage);
+      },
+      runErrands: function() {
+          console.log("Old Mileage:" + this.mileage);
+          this.mileage = this.mileage + 33;
+          console.log("New Mileage: " + this.mileage)
+      }
+    };
+    car.driveAroundWorld();
